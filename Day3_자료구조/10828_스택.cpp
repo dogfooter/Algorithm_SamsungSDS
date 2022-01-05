82a1faf2595d8e7f3867bd7b51c7bb80d0a61fce
Yoon Seyeon <yoon971224@gmail.com>
2022-01-05 09:49:36 +0900

    자료 구조 / 스택

---
+++ Day3_자료구조/10828_스택.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+
+enum OPERATOR { PUSH, POP, SIZE, EMPTY, TOP };
+
+vector<int> intStack;
+int N;
+
+void push()
+{   
+    int X; cin >> X;
+    intStack.push_back(X);
+}
+
+int pop()
+{
+    int num;
+    
+    if (intStack.empty())
+        return -1;
+
+    num = intStack.back() ;
+    intStack.pop_back();
+    return num;
+}
+
+int size()
+{
+    return intStack.size();
+}
+
+int empty()
+{
+    if (intStack.empty()) return 1;
+    else return 0;
+}
+
+int top()
+{
+    if (intStack.empty())
+        return -1;
+    
+    return intStack.back();
+}
+
+int getOperator(string str)
+{
+    if (str.compare("push") == 0) return PUSH;
+    else if (str.compare("pop") == 0) return POP;
+    else if (str.compare("size") == 0) return SIZE;
+    else if (str.compare("empty") == 0) return EMPTY;
+    else if (str.compare("top") == 0) return TOP;
+
+    return -1;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string input;
+        cin >> input;
+        
+        switch (getOperator(input)) {
+            case PUSH:
+                push();
+                break;
+            
+            case POP:
+                cout << pop() << endl;
+                break;
+            
+            case SIZE:
+                cout << size() << endl;
+                break;
+            
+            case EMPTY:
+                cout << empty() << endl;
+                break;
+            
+            case TOP:
+                cout << top() << endl;
+                break;
+            
+            default:
+                cout << "명령어 오류" << endl;
+        }
+    }
+    return 0;
+}
