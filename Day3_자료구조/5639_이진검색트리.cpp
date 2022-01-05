56018beb26543245b1af74b15c8b5caffc3e21b5
Yoon Seyeon <yoon971224@gmail.com>
2022-01-05 23:25:28 +0900

    자료 구조 / 트리 / 재귀

---
+++ Day3_자료구조/5639_이진검색트리.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+};
+
+Node* insert(Node* node, int newData) {
+	if (node == NULL) {
+		node = new Node();
+		node->data = newData;
+		node->left = NULL;
+        node->right = NULL;
+	}
+    // 재귀
+    // 현재 노드의 값보다 작은 경우
+	else if (newData < node->data)
+		node->left = insert(node->left, newData);
+    // 현재 노드의 값보다 큰 경우
+	else if (newData > node->data)
+		node->right = insert(node->right, newData);
+
+	return node;
+}
+
+void postOrder(Node* node) {
+	if (node->left != NULL)
+		postOrder(node->left);
+	if (node->right != NULL)
+		postOrder(node->right);
+	cout << node->data << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Node* root = NULL;
+	int val;
+
+	while (cin >> val) {
+		root = insert(root, val);
+	}
+	postOrder(root);
+
+	return 0;
+}
