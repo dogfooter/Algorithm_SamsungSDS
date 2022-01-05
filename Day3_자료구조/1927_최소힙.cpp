e8062f96127d00078e61f1f9cbc129309a65fe3e
Yoon Seyeon <yoon971224@gmail.com>
2022-01-05 09:50:12 +0900

    자료 구조 / 우선순위 큐

---
+++ Day3_자료구조/1927_최소힙.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    int N;  cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x;  cin >> x;
+        if (x > 0)
+            pq.push(x);
+        else if (x == 0) {
+            if (!pq.empty()) {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else
+                cout << 0 << '\n';
+        }
+    }
+
+    return 0;
+}
