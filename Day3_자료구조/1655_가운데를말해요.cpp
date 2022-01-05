ceede16a0d39f19b52d096f89223b70c574c45d3
Yoon Seyeon <yoon971224@gmail.com>
2022-01-05 10:54:55 +0900

    자료 구조 / 우선순위 큐

---
+++ Day3_자료구조/1655_가운데를말해요.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL);
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int N;
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int num;  cin >> num;
+        
+        // 처음에 값이 없는 경우
+        if (maxHeap.empty()) {
+            maxHeap.push(num);
+        }
+        else {
+            if (maxHeap.size() > minHeap.size())
+                minHeap.push(num);
+            else
+                maxHeap.push(num);
+
+            // 최대 힙의 top이 최소 힙의 top보다 크면 교환
+            if (maxHeap.top() > minHeap.top()) {
+                int maxTop = maxHeap.top();
+                int minTop = minHeap.top();
+                maxHeap.pop();
+                minHeap.pop();
+                maxHeap.push(minTop);
+                minHeap.push(maxTop);
+            }
+        }
+
+        cout << maxHeap.top() << '\n';
+    }
+
+    return 0;
+}
