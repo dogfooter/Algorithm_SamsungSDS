7287a37599ce0d724fc50fa341cce44422f102fc
dogfooter <yoon971224@gmail.com>
2022-01-12 00:47:44 +0900

    DP

---
+++ Day8_동적계획법1/1932_정수삼각형.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define MAX 501
+
+using namespace std;
+
+int n, num;
+int DP[MAX][MAX];
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cin >> num;
+            DP[i][j] = num;
+
+            if (j == 1) {
+                DP[i][j] += DP[i-1][j];
+            }
+            else if (j == i) {
+                DP[i][j] += DP[i-1][j-1];
+            }
+            else {
+                DP[i][j] += max(DP[i-1][j-1], DP[i-1][j]);
+            }
+        }
+    }
+
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        answer = max(answer, DP[n][i]);
+    }
+
+    cout << answer;
+
+    return 0;
+}
